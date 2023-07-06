f7044a1d3ee700ac33a47f68f9d9317cd802c60a
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-06 12:07:07 -0300

    Criando o escopo da classe produto

--- Codc2_POO/cod2/src/entities/Product.java
+++ Codc2_POO/cod2/src/entities/Product.java
@@ -1,4 +1,20 @@
 package entities;
 
 public class Product {
+    public String product;
+    public double price;
+    public int quantity;
+
+    public double TotalValueStock(){
+        return 0.0;
+    }
+
+    public void AddProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void RemoveProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
 }
