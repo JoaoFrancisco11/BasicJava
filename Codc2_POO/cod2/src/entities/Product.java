8cbc9f3144c468f03d896da31058a4cf2e8427d6
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-06 14:24:11 -0300

    Criando os métodos da função Product e criando um retorno do objeto em forma de string

--- Codc2_POO/cod2/src/entities/Product.java
+++ Codc2_POO/cod2/src/entities/Product.java
@@ -1,20 +1,24 @@
 package entities;
 
 public class Product {
-    public String product;
+    public String name;
     public double price;
     public int quantity;
 
-    public double TotalValueStock(){
-        return 0.0;
+    public double totalValueStock(){
+        return quantity*price;
     }
 
-    public void AddProducts(int quantity){
+    public void addProducts(int quantity){
         this.quantity += quantity;
     }
 
     public void RemoveProducts(int quantity){
         this.quantity -= quantity;
+    }
+    public String toString(){
+    return "Product: " + this.name + ", Price: $ " + String.format("%.2f", this.price) + ", Quantity: " + this.quantity
+            + " units, " + "Total: $ " + String.format("%.2f", totalValueStock());
     }
 
 }
