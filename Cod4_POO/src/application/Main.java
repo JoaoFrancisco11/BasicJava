2396076abbfc30b1c16acf0c73653a19f869dcdd
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-06 22:19:03 -0300

    Cod4 finalizada

--- Cod4_POO/src/application/Main.java
+++ Cod4_POO/src/application/Main.java
@@ -1,10 +1,33 @@
 package application;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String Name = sc.nextLine();
+
+        System.out.println("Gross salary: ");
+        double grossSalary = sc.nextDouble();
+
+        System.out.println("Tax: ");
+        double Tax = sc.nextDouble();
+
+        Employee emp = new Employee(Name, grossSalary, Tax);
+        System.out.println(emp);
+
+        System.out.println("Which percentage to increase salary? ");
+        double increase = sc.nextDouble();
+        emp.increaseSalary(increase);
+
+        System.out.println(emp);
+        sc.close();
         }
     }
-}
--- Cod4_POO/src/entities/Employee.java
+++ Cod4_POO/src/entities/Employee.java
@@ -1,4 +1,26 @@
 package entities;
 
 public class Employee {
+    public String Name;
+    public double GrossSalary;
+    public double Tax;
+
+    public Employee(String Name, double GrossSalary, double Tax){
+        this.Name = Name;
+        this.GrossSalary = GrossSalary;
+        this.Tax = Tax;
+    }
+
+    public double setSalary(){
+        return this.GrossSalary - this.Tax;
+
+    };
+
+    public void increaseSalary(double inc){
+        this.GrossSalary *= (1+(inc/100.00));
+    }
+
+    public String toString(){
+        return "Update data: " + this.Name + ", $ " + this.setSalary();
+    }
 }
