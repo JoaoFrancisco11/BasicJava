9e67f87812712205cb871fc50c79569977e8736d
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-07 09:59:24 -0300

    Código 5 finalizado

--- Cod5_POO/src/application/Main.java
+++ Cod5_POO/src/application/Main.java
@@ -1,9 +1,33 @@
 package application;
 
+import entities.Aluno;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Nota 1: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.print("Nota 2: ");
+        double nota2 = sc.nextDouble();
+
+        System.out.print("Nota 3: ");
+        double nota3 = sc.nextDouble();
+
+        Aluno aluno = new Aluno(name, nota1, nota2, nota3);
+        System.out.println(aluno);
+
+        sc.close();
     }
 }
--- Cod5_POO/src/entities/Aluno.java
+++ Cod5_POO/src/entities/Aluno.java
@@ -6,5 +6,35 @@ public class Aluno {
     public double N2;
     public double N3;
     public String Situac;
+    public double Faltante;
+    public double TotalNot;
+
+    public Aluno(String Name, double nota1, double nota2, double nota3){
+        this.Name = Name;
+        this.N1 = nota1;
+        this.N2 = nota2;
+        this.N3 = nota3;
+        this.TotalNot = this.N1+this.N2+this.N3;
+    }
+
+    private void situacaoAluno(){
+        if (TotalNot >= 60){
+            this.Situac = "PASS";
+        }
+        else{
+            this.Situac = "FAILED";
+            this.Faltante = 60.0 - this.TotalNot;
+        }
+    }
+
+    public String toString(){
+        situacaoAluno();
+        if (this.TotalNot >= 60.0){
+            return "FINAL GRADE = " + this.TotalNot + "\r\n" + this.Situac;
+        }
+        else {
+            return "FINAL GRADE = " + this.TotalNot + "\r\n" + this.Situac + "\r\nMissing: " + this.Faltante + "POINTS";
+        }
+    }
 
 }
