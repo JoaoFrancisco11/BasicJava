7a61d5fd2cf4076d6766df8f53011ec338379424
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-07 11:42:22 -0300

    Ajuste dos comentários

--- Cod5_POO/src/application/Main.java
+++ Cod5_POO/src/application/Main.java
@@ -5,8 +5,6 @@ import entities.Aluno;
 import java.util.Locale;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
